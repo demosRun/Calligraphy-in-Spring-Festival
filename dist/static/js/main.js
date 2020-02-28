4868176151cce3a675dd98288f2cdd722d5a1872
puge <mail@puge.org>
2020-02-28 16:44:30 +0800

    save

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -2,3 +2,8 @@
 document.body.addEventListener('touchmove', function (e) {
   e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
 }, {passive: false})
+
+// 微信自动播放音乐
+document.addEventListener("WeixinJSBridgeReady", function () {
+  owo.script.playMusic.play()
+}, false)
